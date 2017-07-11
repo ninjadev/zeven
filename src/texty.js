006e77fcc65b67d5be306eb1e9ec75dd72051624
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-07-11 16:49:24 +0200

    Add animations to logo

--- src/texty.js
+++ src/texty.js
@@ -1,44 +1,135 @@
 (function(global) {
-  class texty extends NIN.Node {
+  class texty extends NIN.ShaderNode {
     constructor(id, options) {
       super(id, {
+        inputs: {
+          image: new NIN.TextureInput(),
+        },
         outputs: {
           render: new NIN.TextureOutput()
-        }
+        },
+        shader: 'Add',
       });
 
       this.canvas = document.createElement('canvas');
       this.ctx = this.canvas.getContext('2d');
       this.canvasTexture = new THREE.Texture(this.canvas);
+      this.canvasTexture.minFilter = THREE.LinearFilter;
+      this.canvasTexture.magFilter = THREE.LinearFilter;
       this.resize();
+
+      const step = 5 / 11;
+      this.linePoints = [
+        /* horizontal */
+        0, 0,
+        37.5, 0,
+
+        0, 2,
+        37.5, 2,
+
+        0, 3,
+        37.5, 3,
+
+        0, 5,
+        37.5, 5,
+
+        0, 7,
+        37.5, 7,
+
+        0, 9,
+        37.5, 9,
+
+        0, 11,
+        37.5, 11,
+
+        0, 13,
+        37.5, 13,
+
+        /* forward diagonal */
+        6 + step * 2, 0,
+        6 - step * 11, 13,
+
+        8 + step * 3, 0,
+        8 - step * 10, 13,
+
+        9 + step * 3, 0,
+        9 - step * 10, 13,
+
+        11 + step * 3, 0,
+        11 - step * 10, 13,
+
+        16.5 + step * 3, 0,
+        16.5 - step * 10, 13,
+
+        19 + step * 3, 0,
+        19 - step * 10, 13,
+
+        21 + step * 3, 0,
+        21 - step * 10, 13,
+
+        23.5 + step * 3, 0,
+        23.5 - step * 10, 13,
+
+        29 + step * 3, 0,
+        29 - step * 10, 13,
+
+        31.5 + step * 3, 0,
+        31.5 - step * 10, 13,
+
+        35 + step * 3, 0,
+        35 - step * 10, 13,
+
+        37.5 + step * 3, 0,
+        37.5 - step * 10, 13,
+
+        /* vertical */
+        0, 0,
+        0, 13,
+
+        2, 0,
+        2, 13,
+
+        6, 0,
+        6, 13,
+
+        8.2, 0,
+        8.2, 13,
+      ];
     }
 
     update(frame) {
+      this.frame = frame;
+      this.uniforms.opacity.value = 1;
+      this.uniforms.A.value = this.inputs.image.getValue();
+      this.uniforms.B.value = this.canvasTexture;
     }
 
     render(renderer) {
-      this.ctx.fillStyle = '#4444ff';
       this.ctx.save();
       this.ctx.scale(GU, GU);
+      this.ctx.fillStyle = 'black';
       this.ctx.fillRect(0, 0, 16, 9);
       this.ctx.scale(1 / 4, 1 / 4);
 
-      this.ctx.fillStyle = '#aaf';
+      /*
       for(let x = 0; x < 16 * 4; x++) {
         this.ctx.fillRect(x, 0, 0.02, 9 * 4);
       }
       for(let y = 0; y < 9 * 4; y++) {
         this.ctx.fillRect(0, y, 16 * 4, 0.02);
       }
+      */
 
-      this.ctx.translate(5, 5);
+      this.ctx.translate(13.5, 10);
 
-      this.fillStyle = 'white';
-      this.strokeStyle = 'black';
+      this.ctx.fillStyle = 'white';
+      this.ctx.trokeStyle = 'black';
+      this.ctx.globalAlpha = BEAN >= 804 ? 1 : 0;
 
       const step = 5 / 11;
 
       /* 7 */
+      this.ctx.beginPath();
       this.ctx.moveTo(0, 0);
       this.ctx.lineTo(8 + Math.sin(step * Math.PI * 2), 0);
       this.ctx.lineTo(8 + Math.sin(step * Math.PI * 2), 2);
@@ -109,10 +200,12 @@
 
       this.ctx.fill();
       this.ctx.lineWidth = 0.2;
-      //this.ctx.stroke();
+      this.ctx.strokeStyle = 'white';
+      this.ctx.stroke();
 
       this.ctx.beginPath();
-      this.ctx.fillStyle = 'black';
+      this.ctx.strokeStyle = 'black';
+      this.ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
       this.ctx.save();
       this.ctx.translate(16, 3);
       this.ctx.moveTo(0.5, 0);
@@ -125,21 +218,59 @@
       this.ctx.lineTo(3 - step * 2, 2);
       this.ctx.lineTo(3 - step * 3.0, 3.0);
       this.ctx.lineTo(0.5 - step * 3.0, 3.0);
+      this.ctx.lineTo(0.5, 0);
       this.ctx.restore();
+      this.ctx.stroke();
       this.ctx.fill();
 
+      this.ctx.strokeStyle = 'white';
+      this.ctx.fillStyle = 'white';
+      this.ctx.lineWidth = 0.1;
+      for(let i = 0; i < this.linePoints.length; i+= 8) {
+        this.ctx.globalAlpha = easeOut(.5, 0, (this.frame - 2295) / 60);
+        this.ctx.beginPath();
+        const dx0 = this.linePoints[i] - this.linePoints[i + 2];
+        const dy0 = this.linePoints[i + 1] - this.linePoints[i + 3];
+        const dx1 = this.linePoints[i + 4] - this.linePoints[i + 4 + 2];
+        const dy1 = this.linePoints[i + 4 + 1] - this.linePoints[i + 4 + 3];
+        let incline0 = dx0 / dy0;
+        let incline1 = dx1 / dy1;
+        const distance = 60;
+        let animation = smoothstep(0, 1, (this.frame - 2193 - i * 0.95 + 40) / 90);
+        if(dy0 == 0) {
+          this.ctx.moveTo(this.linePoints[i] - distance, this.linePoints[i + 1]);
+          this.ctx.lineTo(this.linePoints[i] - distance + 2 * distance * animation, this.linePoints[i + 1]);
+          this.ctx.lineTo(this.linePoints[i+4] - distance + 2 * distance * animation, this.linePoints[i+4+ 1]);
+          this.ctx.lineTo(this.linePoints[i+4] - distance, this.linePoints[i+4 + 1]);
+          this.ctx.lineTo(this.linePoints[i] - distance, this.linePoints[i + 1]);
+        } else {
+          this.ctx.moveTo(this.linePoints[i] - distance * incline0, this.linePoints[i + 1] - distance);
+          this.ctx.lineTo(this.linePoints[i] - distance * incline0 + 2 * distance * incline0 * animation, this.linePoints[i + 1] -distance + 2 * distance * animation);
+          this.ctx.lineTo(this.linePoints[i+4] - distance * incline1 + 2 * distance * incline1 * animation, this.linePoints[i+4 + 1] -distance + 2 * distance * animation);
+          this.ctx.lineTo(this.linePoints[i+4] - distance * incline0, this.linePoints[i+4 + 1] - distance);
+          this.ctx.lineTo(this.linePoints[i] - distance * incline0, this.linePoints[i + 1] - distance);
+        }
+        this.ctx.stroke();
+        this.ctx.globalAlpha *= 0.5;
+        this.ctx.fill();
+      }
+
+
       this.ctx.restore();
 
-
-      this.outputs.render.setValue(this.canvasTexture);
       this.canvasTexture.needsUpdate = true;
+      super.render(renderer);
     }
 
     resize() {
-      this.canvas.width = 16 * GU;
-      this.canvas.height = 9 * GU;
+      if(this.canvas) {
+        this.canvas.width = 16 * GU;
+        this.canvas.height = 9 * GU;
+      }
+      super.resize();
     }
   }
 
   global.texty = texty;
+
 })(this);
