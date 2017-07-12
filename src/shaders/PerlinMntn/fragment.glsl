3c2678b64b17bde6af717da9aa1c16a019bec11b
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2017-07-12 02:03:17 +0200

    Change mntn lighting to match skybox. And change mntn rotation

--- src/shaders/PerlinMntn/fragment.glsl
+++ src/shaders/PerlinMntn/fragment.glsl
@@ -12,7 +12,7 @@ void main() {
 
     vec4 cliffSample = texture2D(tCliff,vUv.xy * 7.0);
     //z coordinate is height in model space
-    vec3 material = (abs(vNormal.z) > length(cliffSample) - 0.3)
+    vec3 material = (abs(vNormal.y) > length(cliffSample) - 0.3)
         ? 0.2 * vec3(1.0,1.0,1.0)
         : 0.2 * cliffSample.rgb * cliffSample.rgb ;
 
@@ -21,21 +21,23 @@ void main() {
 
     // NOTE: Normals are calculated when the plane is sideways
     // So for lights z and y axes are swapped
-    vec3 normal =  normalize(vNormal+cliffSample.xyz);
+    //vec3 normal =  normalize(vNormal+cliffSample.xyz);
+    vec3 normal =  normalize(vNormal);
 
 
     //Sun light
-    vec3 sunDir = normalize(vec3(0.0, 1.0, 0.0));
-    vec3 sunColor = vec3(1.64,1.27,0.99);
+    vec3 sunDir = normalize(vec3(5.0, 2.66, -5.0));
+    //vec3 sunColor = vec3(1.64,1.27,0.99); //iq
+    vec3 sunColor = 5.64 * vec3(1.0, 0.98823, 0.9058); //skymap data
     float sunIntensity = clamp( dot(sunDir, normal), 0.0, 1.0);
 
     //Sky light
-    vec3 skyDir = normalize(vec3(0.0, 0.0, 1.0));
-    vec3 skyColor = vec3(0.16,0.20,0.28);
-    float skyIntensity = clamp( dot(skyDir, normal), 0.0, 1.0);
+    //vec3 skyColor = vec3(0.16,0.20,0.28); //iq
+    vec3 skyColor = vec3(0.2109375, 0.236328125, 0.29296875); //skymap data
+    float skyIntensity = clamp( 0.5 + 0.5 * normal.y, 0.0, 1.0);
 
     //Fake indirect sun light
-    vec3 indSunDir = normalize(vec3(0.0, -1.0, 0.0));
+    vec3 indSunDir = normalize(sunDir*vec3(-1.0,0.0,-1.0));
     vec3 indSunColor = vec3(0.40,0.28,0.20);
     float indSunIntensity = clamp( dot(indSunDir, normal), 0.0, 1.0);
 
@@ -53,6 +55,6 @@ void main() {
 
     color = mix(color, fogColor, fogAmount);
 
-    //color = pow(color, vec3(1.0/2.2));
+    color = pow(color, vec3(1.0/2.2));
     gl_FragColor = vec4(color, 1.0);
 }
