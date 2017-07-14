1eef3ce9736680acd69e5737a58c75443947e94e
Rune Holmgren <Raane.Holm@gmail.com>
2017-07-14 22:25:16 +0200

    Add sunX, sunY and amount as parameters for the lens flare effect.

--- src/LensFlareNode.js
+++ src/LensFlareNode.js
@@ -13,31 +13,9 @@
       this.uniforms.frame.value = frame;
       this.uniforms.tDiffuse.value = this.inputs.image.getValue();
 
-      /*this.uniforms.sunX.value = 0.8;
-      this.uniforms.sunY.value = 0.8;
-      this.uniforms.amount.value = 0.4;*/
-
-    /*var relativeFrame = frame - this.config.startFrame;
-    var endRelativeFrame = this.config.endFrame - frame; 
-    this.shaderPass.uniforms.width.value = 16 * 8;
-    this.shaderPass.uniforms.height.value = 9 * 8;
-    this.shaderPass.uniforms.time.value = frame;
-    this.shaderPass.uniforms.amount.value = lerp(0, 1.0, Math.min(Math.min(relativeFrame,endRelativeFrame) / 70, 1));
-    //this.shaderPass.uniforms.sunX.value = lerp(1.0,0.3, relativeFrame/(endRelativeFrame-relativeFrame));
-    console.log(frame);
-    if(frame<1820) {
-      this.shaderPass.uniforms.sunX.value = 
-          -0.2 - (1820- frame)/500;
-          //0.9 - 1.5*Math.pow(endRelativeFrame/(this.config.endFrame-this.config.startFrame), 2.6);
-      this.shaderPass.uniforms.sunY.value = 
-          0.75 + (1820 - frame)/200;
-    } else if (frame<1906) {
-      this.shaderPass.uniforms.sunX.value = -0.2;
-      this.shaderPass.uniforms.sunY.value = 0.75;
-    } else {
-      this.shaderPass.uniforms.sunX.value = -0.2  + Math.pow((frame - 1906),2.5)/Math.pow(180,2.5)*0.6;
-      this.shaderPass.uniforms.sunY.value = 0.75 + Math.pow((frame - 1906),1.5)/Math.pow(180,1.5)*0.4;
-    }*/
+      this.uniforms.sunX.value = Math.sin(frame/40)/2 + .5;
+      this.uniforms.sunY.value = Math.cos(frame/40)/2 + .5;
+      this.uniforms.amount.value = Math.sin(frame/80)/3 + .5;;
     }
   }
 
